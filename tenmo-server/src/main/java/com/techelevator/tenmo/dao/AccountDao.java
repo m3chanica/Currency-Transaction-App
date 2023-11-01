d800da2eafae4dbb9852b076ffa62bd408994ce4
Matthew Pallenik <mpallenik@gmail.com>
2023-11-01 15:12:49 -0400

    Added account, accountService, etc.

---
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/AccountDao.java
@@ -0,0 +1,20 @@
+package com.techelevator.tenmo.dao;
+
+import com.techelevator.tenmo.model.Account;
+import com.techelevator.tenmo.model.RegisterUserDto;
+import com.techelevator.tenmo.model.User;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public interface AccountDao {
+
+    List<Account> getAccounts();
+
+    Account getAccountByAccountId(int accountId);
+
+    Account getAccountByUsername(String username);
+
+    BigDecimal getBalanceByUsername(String username);
+
+}
